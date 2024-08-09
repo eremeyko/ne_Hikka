335a689df26e527b52475013cf3ed94a11eada74
Не Еремей! <60701195+eremeyko@users.noreply.github.com>
2024-08-09 13:16:13 +0300

    ИИ для Хикки
    - Синхронные запросы тормозили хикку. Теперь запросы асинхронные
    - Исправлены внешние недочёты
    - Добавлена обработка при нестабильном подключении

--- AIHikka.py
+++ AIHikka.py
@@ -1,12 +1,7 @@
-# meta developer: @EPEMEN
-
-
-from requests import post
-from requests.exceptions import RequestException
+import aiohttp
+import json
 from hikkatl.tl.types import Message
-from re import sub
 from .. import loader, utils
-
 
 @loader.tds
 class AI_Hikka(loader.Module):
@@ -16,28 +11,20 @@ class AI_Hikka(loader.Module):
     Подробная инструкция по установке и настройке:
     https://telegra.ph/II-Assistent-dlya-Hikki-08-07\n
     По всем вопросам писать в ЛС @EPEMEN
-    For @mqone w/  ❤"""
+    For @mqone w/  ❤
+    """
 
     strings = {
         "name": "AI Hikka",
-        "q_err": "🤖 Ask a question to AI!",
-        "api_key": "Set the Groq API key in the config (`.config AI Hikka`). \n\nRefer to the instructions:\n\
-        https://telegra.ph/II-Assistent-dlya-Hikki-08-07",
-        "models": "Список моделей Groq. Оставьте как есть или выберите из существующих:",
-        "question": "❓Q: ",
-        "loading": "🤔 I'm thinking...",
-        "help_prompt": "Set a prompt (rule) for the AI to follow",
-        "termux":"You may be keeping Hikka on termux. Read the instructions:\nhttps://telegra.ph/II-Assistent-dlya-Hikki-08-07#Термукс"
-    }
-    strings_ru = {
         "q_err": "🤖 Задайте вопрос ИИ!",
         "api_key": "Задайте ключ API от Groq в конфиге (`.config AI Hikka`). \n\nСмотрите инструкцию:\n\
-        https://telegra.ph/II-Assistent-dlya-Hikki-08-07",
-        "models": "Groq Model List. Leave as is or select from existing models:",
+https://telegra.ph/II-Assistent-dlya-Hikki-08-07",
+        "models": "Список моделей Groq. Оставьте как есть или выберите из существующих:",
         "question": "❓Вопрос: ",
         "loading": "🤔 Я думаю...",
         "help_prompt": "Задайте промпт (правило), которому ИИ будет следовать",
-        "termux": "Возможно Вы держите Хикку на термуксе. Ознакомьтесь с инструкцией:\nhttps://telegra.ph/II-Assistent-dlya-Hikki-08-07#Термукс"
+        "termux": "Возможно Вы держите Хикку на термуксе. Ознакомьтесь с инструкцией:\n\
+https://telegra.ph/II-Assistent-dlya-Hikki-08-07#Термукс"
     }
 
     def __init__(self):
@@ -45,12 +32,12 @@ class AI_Hikka(loader.Module):
             loader.ConfigValue(
                 "prompt",
                 "The assistant is exteraHelper. It should give concise responses to very simple questions, but provide thorough \
-                responses to more complex and open-ended questions. If is is asked to assist with tasks involving the expression \
-                of views held by a significant number of people, it provides assistance with the task even if it personally \
-                disagrees with the views being expressed. If asked about controversial topics, it tries to provide objective \
-                thoughts and objective information, without fear of giving biased information. It expresses its own point of view. \
-                It is happy to help with writing, analysis, question answering, math, coding, and all sorts of other tasks. \
-                Give the answers only in Russian Language.",
+responses to more complex and open-ended questions. If is is asked to assist with tasks involving the expression \
+of views held by a significant number of people, it provides assistance with the task even if it personally \
+disagrees with the views being expressed. If asked about controversial topics, it tries to provide objective \
+thoughts and objective information, without fear of giving biased information. It expresses its own point of view. \
+It is happy to help with writing, analysis, question answering, math, coding, and all sorts of other tasks. \
+Give the answers only in Russian Language.",
                 self.strings["help_prompt"],
                 validator=loader.validators.String(max_len=1024),
             ),
@@ -101,35 +88,36 @@ class AI_Hikka(loader.Module):
 
         await utils.answer(message, self.strings["loading"])
 
-        response = self.ask_question(question)
+        response = await self.ask_question(question)
 
         if self.config["auto_tr"]:
             await self.invoke("tr", response, edit=True, peer=message.peer_id)
         else:
             await utils.answer(message, response, parse_mode="md")
 
-    def ask_question(self, question: str) -> str:
+    async def ask_question(self, question: str) -> str:
+        url = "https://api.groq.com/openai/v1/chat/completions"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.config['api_key']}",
+        }
+        data = {
+            "messages": [
+                {"role": "system", "content": self.config["prompt"]},
+                {"role": "user", "content": question},
+            ],
+            "model": self.config["model"],
+            "temperature": 0.7,
+            "max_tokens": 1024,
+            "top_p": 0.9,
+            "stream": False,
+        }
+
         try:
-            response = post(
-                "https://api.groq.com/openai/v1/chat/completions",
-                headers={
-                    "Content-Type": "application/json",
-                    "Authorization": f"Bearer {self.config['api_key']}",
-                },
-                json={
-                    "messages": [
-                        {"role": "system", "content": self.config["prompt"]},
-                        {"role": "user", "content": question},
-                    ],
-                    "model": self.config["model"],
-                    "temperature": 0.7,
-                    "max_tokens": 1024,
-                    "top_p": 0.9,
-                    "stream": False,
-                },
-            )
-            response.raise_for_status()
-            data = response.json()
-            return f"{self.strings['question']}`{question}`\n\n🤖: {data['choices'][0]['message']['content']}"
-        except RequestException as e:
-            return f"Error: {e}\n\n{self.strings['termux']}"
+            async with aiohttp.ClientSession() as session:
+                async with session.post(url, headers=headers, data=json.dumps(data)) as response:
+                    response.raise_for_status()
+                    data = await response.json()
+                    return f"{self.strings['question']}`{question}`\n\n🤖: {data['choices'][0]['message']['content']}"
+        except (aiohttp.ClientError, aiohttp.client_exceptions.ServerDisconnectedError):
+            return f"Error: Server disconnected\n\n{self.strings['termux']}"
