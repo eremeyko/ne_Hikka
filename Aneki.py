b362fbcd955759366f9f29272e528094cd6d0de7
Не Еремей! <60701195+eremeyko@users.noreply.github.com>
2024-09-10 13:32:22 +0300

    Анеки)
    +канал

--- Aneki.py
+++ Aneki.py
@@ -12,7 +12,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 # Original repository: https://github.com/eremeyko/ne_Hikka
-# meta developer: @EPEMEN
+# meta developer: @eremod
 from requests import get
 from random import choice
 from hikkatl.types import Message
